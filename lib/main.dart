c554dc965b8c50cf5718791a3f51e0315fd4859f
khurshid <95979088+khurshid28@users.noreply.github.com>
2022-01-23 03:19:35 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
-
-
-                //   // final byteData = await rootBundle.load('assets/'+names[index]+".jpg");
-                //   Directory? _dir = await getExternalStorageDirectory();
-
-                //   var dbPath = join(_dir!.path, 'assets/'+names[index]+".jpg");
-                //   ByteData data = await rootBundle.load('assets/'+names[index]+".jpg");
-                //   List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-                //   final my_image_file= await File(dbPath).writeAsBytes(bytes);
-                //    // String appDocPath = _dir.path;
-
-                //   // print(appDocPath);
-          
-                //  await GallerySaver.saveImage(my_image_file.path,albumName: "footballers");
-                //  print('Image is saved');
